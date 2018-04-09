032f18e938c1a3a6e19632dad6cd1245db028200
chaibs <chaibanseok@gmail.com>
2018-04-09 23:19:39 +0900

    20180409

--- projecteuler.py
+++ projecteuler.py
@@ -1,4 +1,19 @@
+# 20180409
 # Multiples of 3 and 5
 # 1000 이하에서 3과 5의 배수를 찾아 합을 구하라.
 
+result = []
+n = 0
 
+while n < 1000:
+	if n % 3 == 0 or n % 5 == 0:
+		result.append(n)
+	else:
+		pass
+	n = n + 1
+
+
+sum(result)
+
+# while문에서 n을 그냥 써도 되는 줄 알앗듬...
+# or 연산자 | 아니고 그냥 or
